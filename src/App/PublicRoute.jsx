8298a16fa434a3e771a87558bcf194bb8e099116
Vladislav Boiko <90558354+Valdesrep@users.noreply.github.com>
2022-07-21 23:15:04 +0300

    1

--- src/App/PublicRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from 'react-router';
-import { useSelector } from 'react-redux';
-import authSelectors from '../redux/auth/authSelectors';
-
-export default function PublicRoute() {
-  const isLoggetIn = useSelector(authSelectors.getIsLoggedIn);
-
-  return isLoggetIn ? <Navigate to="contacts" /> : <Outlet />;
-}
